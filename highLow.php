131de168db0faa713789589832260ba980d4bcc4
Tarek S Hafez <thafez@ymail.com>
2015-07-13 14:26:11 -0500

    Modified highLow.php to restart gameplay and track number of guess attempts.

--- highLow.php
+++ highLow.php
@@ -1,39 +1,46 @@
 <?php
 
-
 	function highLowLogic () {
 		$randomNumber = rand(1, 100);
 		fwrite(STDOUT, "Guess a Number!" . PHP_EOL);
 		$guess = fgets(STDIN);
+		$guessCount = 0;
 		
 		do {
 			if ($guess < $randomNumber) {
 				fwrite(STDOUT, "HIGHER!" . PHP_EOL);
 				$guess = fgets(STDIN);
+				$guessCount += 1;
 			} else if ($guess > $randomNumber) {
 				fwrite(STDOUT, "LOWER!" . PHP_EOL);
 				$guess = fgets(STDIN);
+				$guessCount += 1;
 			}
 		} while ($guess != $randomNumber);
 
-		function playAgain () {
-			$askReplay = true;
-			if ($askReplay == true) {
-				fwrite(STDOUT, "Would you like to play again? y/n" . PHP_EOL);
-				$replay = trim(fgets(STDIN));
-				if ($replay == "y") {
-					$askReplay = false;
-					highLowLogic();
-				}
-			}
-			$replay = false;
-		}
 
 		if ($guess == $randomNumber) {
+				$guessCount += 1;
+				echo("You made {$guessCount} guesses!" . PHP_EOL);
+				$guessCount = 0;
 				fwrite(STDOUT, "GOOD GUESS! You won!" . PHP_EOL);
 				playAgain();
 		}
 		
+	}
+	
+	function playAgain () {
+		$askReplay = true;
+
+		if ($askReplay == true) {
+			fwrite(STDOUT, "Would you like to play again? y/n" . PHP_EOL);
+			$replay = trim(fgets(STDIN));
+			if ($replay == "y") {
+				$askReplay = false;
+				highLowLogic();
+			}
+		}
+		$replay = false;
 	}
 
 	highLowLogic();
