3213449ddd5727368ce1027a65285dbb00b4a10d
Tarek S Hafez <thafez@ymail.com>
2015-07-14 10:20:34 -0500

    Refactored highLow.php to accept user arguments.

--- highLow.php
+++ highLow.php
@@ -1,50 +1,71 @@
 <?php
-	
+
+
 	// Initial Game Prompting to start game logic.
 	fwrite(STDOUT, "Do you wanna play a game? y/n" . PHP_EOL);
 	$play = trim(fgets(STDIN));	
 	if ($play = "y") {
-		highLowLogic();
+		if ($argc === 3) {
+			fwrite(STDOUT, "Guess a number between {$argv[1]} and {$argv[2]}!" . PHP_EOL);			
+			$random = randomNumberSet($argv[1], $argv[2]);
+		} else if ($argc == 1) {
+			fwrite(STDOUT, "Please provide a minimum starting number:" . PHP_EOL);
+			$minimum = trim(fgets(STDIN));
+			fwrite(STDOUT, "Please provide a maximum starting number:" . PHP_EOL);
+			$maximum = trim(fgets(STDIN));
+			fwrite(STDOUT, "Guess a number between $minimum and $maximum!" . PHP_EOL);			
+			$random = randomNumberSet($minimum, $maximum);
+		}
+
+		highLowLogic($random);
 	}
 
-	// Game logic driver
-	function highLowLogic () {
-		$randomNumber = mt_rand(1, 100);
-		fwrite(STDOUT, "Guess a number between 1 and 100!" . PHP_EOL);
-		$guess = fgets(STDIN);
+	// Random Number Generator responsive to user input.
+	function randomNumberSet ($minimum, $maximum) {
+		$randomNumber = mt_rand($minimum, $maximum);
+		return $randomNumber;
+	}
+	// Game logic function
+	function highLowLogic ($random) {
 		$guessCount = 0;
+		$guess = fgets(STDIN);
 		
-		// comparison of user input to randomly guessed number
+		// comparison of user input to randomly generated number
 		do {
-			if ($guess < $randomNumber) {
+			if ($guess < $random) {
 				fwrite(STDOUT, "HIGHER!" . PHP_EOL);
 				$guess = fgets(STDIN);
 				$guessCount += 1;
-			} else if ($guess > $randomNumber) {
+			} else if ($guess > $random) {
 				fwrite(STDOUT, "LOWER!" . PHP_EOL);
 				$guess = fgets(STDIN);
 				$guessCount += 1;
 			}
-		} while ($guess != $randomNumber);
+		} while ($guess != $random);
 		
-		//  victory condition and introduction of playAgain function
-		if ($guess == $randomNumber) {
-				$guessCount += 1;
-				echo("You made {$guessCount} guesses!" . PHP_EOL);
-				$guessCount = 0;
-				fwrite(STDOUT, "GOOD GUESS! You won!" . PHP_EOL);
-				playAgain();
+		//  victory condition and implementation of playAgain function
+		if ($guess == $random) {
+			$guessCount += 1;
+			fwrite(STDOUT, "GOOD GUESS!" . PHP_EOL);
+			echo("You won, with only {$guessCount} guesses!" . PHP_EOL);
+			$guessCount = 0;
+			playAgain();
 		}	
 	}
 	
 	// Definition of playAgain function
 	function playAgain () {
-			fwrite(STDOUT, "Would you like to play again? y/n" . PHP_EOL);
-			$replay = trim(fgets(STDIN));
-			if ($replay == "y") {
-				$askReplay = false;
-				highLowLogic();
-			}
+		fwrite(STDOUT, "Would you like to play again? y/n" . PHP_EOL);
+		$replay = trim(fgets(STDIN));
+		if ($replay == "y") {
+			fwrite(STDOUT, "Please provide a minimum starting number:" . PHP_EOL);
+			$minimum = trim(fgets(STDIN));
+			fwrite(STDOUT, "Please provide a maximum starting number:" . PHP_EOL);
+			$maximum = trim(fgets(STDIN));
+			fwrite(STDOUT, "Guess a number between $minimum and $maximum!" . PHP_EOL);
+			$random = randomNumberSet($minimum, $maximum);
+			highLowLogic($random);
+		}
 	}
 
 ?>
